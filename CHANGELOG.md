d6b0d5e06cf033e32319b119c8371affee3b4dcc
Geuthur <geuthur@users.noreply.github.com>
2024-07-05 09:17:33 +0200

    First Initial Commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## \[1.0.0\] - 2024-07-05
+
+### Added
+
+- Initial public release
---
+++ madashboard/views.py
@@ -0,0 +1,27 @@
+from django.template.loader import render_to_string
+
+# Alliance Auth
+from allianceauth.services.hooks import get_extension_logger
+from allianceauth.authentication.models import CharacterOwnership
+
+logger = get_extension_logger(__name__)
+
+def dashboard_memberaudit_check(request):
+    unregistered = CharacterOwnership.objects.filter(
+        user=request.user, character__memberaudit_character__isnull=True
+    )
+
+    chars = {}
+
+    for char in unregistered:
+        chars[char.character.character_id] = {
+            "id": char.character.character_id,
+            "name": char.character.character_name,
+        }
+
+    context = {
+        "chars": chars if chars else None,
+    }
+    return render_to_string(
+        "madashboard/dashboard.html", context=context, request=request
+    )
