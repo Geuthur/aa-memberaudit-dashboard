b6d21df9aad32d0e9818710aadf9ae26eee6d757
Geuthur <geuthur@users.noreply.github.com>
2025-10-15 13:32:45 +0200

    [CHANGE] Update Makefile

--- .make/conf.d/pre-commit.mk
+++ .make/conf.d/pre-commit.mk
@@ -1,15 +1,3 @@
-# Run pre-commit checks
-.PHONY: pre-commit-checks
-pre-commit-checks: pre-commit-install
-	@echo "Running pre-commit checks …"
-	@pre-commit run --all-files
-
-# Update pre-commit configuration
-.PHONY: pre-commit-update
-pre-commit-update:
-	@echo "Updating pre-commit configuration …"
-	@pre-commit autoupdate
-
 # Install pre-commit hook
 .PHONY: pre-commit-install
 pre-commit-install:
@@ -22,12 +10,24 @@ pre-commit-uninstall:
 	@echo "Uninstalling pre-commit hook …"
 	@pre-commit uninstall
 
+# Update pre-commit configuration
+.PHONY: pre-commit-update
+pre-commit-update:
+	@echo "Updating pre-commit configuration …"
+	@pre-commit autoupdate --freeze
+
+# Run pre-commit checks
+.PHONY: pre-commit-checks
+pre-commit-checks: pre-commit-install
+	@echo "Running pre-commit checks …"
+	@pre-commit run --all-files
+
 # Help message for the Pre-Commit commands
 .PHONY: help
 help::
-	@echo "  $(TEXT_UNDERLINE)pre-commit:$(TEXT_UNDERLINE_END)"
-	@echo "    pre-commit-checks         Run pre-commit checks"
-	@echo "    pre-commit-install        Install pre-commit hook"
-	@echo "    pre-commit-uninstall      Uninstall pre-commit hook"
-	@echo "    pre-commit-update         Update pre-commit configuration"
+	@echo "  $(TEXT_UNDERLINE)Pre-Commit:$(TEXT_UNDERLINE_END)"
+	@echo "    pre-commit-install          Install pre-commit hook"
+	@echo "    pre-commit-uninstall        Uninstall pre-commit hook"
+	@echo "    pre-commit-update           Update pre-commit configuration"
+	@echo "    pre-commit-checks           Run pre-commit checks"
 	@echo ""
